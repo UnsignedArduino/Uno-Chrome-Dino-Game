342c448ea2647175a5010a42e02fdaa361323674
UnsignedArduino <UnsignedArduino@outlook.com>
2022-03-13 13:34:35 -0400

    Can print millis to LCD

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
 #include <Arduino.h>
+#include <LiquidCrystal.h>
+#include "pinouts.h"
+
+const byte width = 16;
+const byte height = 2;
+
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+  lcd.begin(width, height);
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
+  lcd.clear();
+  lcd.home();
+  lcd.print(millis());
+  delay(50);
 }
---
+++ src/pinouts.h
@@ -0,0 +1,9 @@
+#include <Arduino.h>
+
+// LCD pins, using 4-bit mode
+const byte rs = 8;
+const byte en = 9;
+const byte d4 = 4;
+const byte d5 = 5;
+const byte d6 = 6;
+const byte d7 = 7;
